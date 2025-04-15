62fc4a93605b2333ee3c4e1438b3fc73550e9429
c0a241863c <c0a241863c@edu.teu.ac.jp>
2025-04-15 17:42:33 +0900

    演習１：こうかとん左に移動

--- pygame_image.py
+++ pygame_image.py
@@ -19,6 +19,7 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+
         key_lst = pg.key.get_pressed()#練習10-3
         if key_lst[pg.K_UP]:
            kk_rct.move_ip((0, -1))#練習10-4
@@ -27,9 +28,10 @@ def main():
         if key_lst[pg.K_LEFT]:
            kk_rct.move_ip((-1, 0))
         if key_lst[pg.K_RIGHT]:
-           kk_rct.move_ip((+1, 0))
+           kk_rct.move_ip((+2, 0))
 
         x=tmr%3200#練習6,9
+        kk_rct.move_ip((-1, 0))
         screen.blit(bg_img, [-x, 0])#練習6
         screen.blit(bg_img2, [-x+1600, 0])#練習7
         screen.blit(bg_img, [-x+3200, 0])#練習9

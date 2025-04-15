616d36b6f143966cd6248d916672e0e36cf97282
c0a241863c <c0a241863c@edu.teu.ac.jp>
2025-04-15 18:07:12 +0900

    move1回

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -21,17 +21,23 @@ def main():
             if event.type == pg.QUIT: return
 
         key_lst = pg.key.get_pressed()#練習10-3
+        idou = (-1,0)
         if key_lst[pg.K_UP]:
-           kk_rct.move_ip((0, -1))#練習10-4
+           #kk_rct.move_ip((0, -1))#練習10-4
+           idou = (0,-1)
         if key_lst[pg.K_DOWN]:
-           kk_rct.move_ip((0, +1))
+           #kk_rct.move_ip((0, +1))
+            idou=(0,+1)
         if key_lst[pg.K_LEFT]:
-           kk_rct.move_ip((-1, 0))
+         #   kk_rct.move_ip((-1, 0))
+            idou=(-1,0)
         if key_lst[pg.K_RIGHT]:
-           kk_rct.move_ip((+2, 0))
+         #   kk_rct.move_ip((+2, 0))
+            idou=(+2,0)
 
+        
         x=tmr%3200#練習6,9
-        kk_rct.move_ip((-1, 0))
+        kk_rct.move_ip(idou)
         screen.blit(bg_img, [-x, 0])#練習6
         screen.blit(bg_img2, [-x+1600, 0])#練習7
         screen.blit(bg_img, [-x+3200, 0])#練習9
